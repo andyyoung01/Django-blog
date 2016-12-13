7414e91d546a9f4d03913e20d046df75ba6f8d42
andyyoung01 <andyyoung01@gmail.com>
2016-12-13 16:13:35 +0800

    finished chapter08

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,11 @@ from blog import views
 
 from blog.models import Post
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list, name='post_list'),
     url(r'^post/([0-9]+)/$', views.post_detail, name='post-detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/([0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/([0-9]+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/([0-9]+)/remove/$', views.post_remove, name='post_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,27 @@ from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post
 from blog.forms import PostForm
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    #postsAll = Post.objects.annotate(num_comment=array.count('comment')).filter(
+    #    published_date__isnull=False).prefetch_related(
+    #    'category').prefetch_related('tags').order_by('-published_date')
+    postsAll = Post.objects.filter(published_date__isnull=False).order_by('-created_date')
+    #for p in postsAll:
+    #    p.click = cache_manager.get_click(p)
+    paginator = Paginator(postsAll, 10)  # Show 10 contacts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post_list.html', {'posts': posts, 'page': True})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -36,3 +52,17 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post-detail', post.id)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
